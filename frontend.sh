1eb04529f2db60dd9a1f6b824b627c99a0c82723
manibhumaraju <manishvarmab@gmail.com>
2024-09-02 17:42:55 +0530

    shell

---
+++ frontend.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+USERID=$(id -u)
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOG_FOLDER
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this Script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is....$R Failed $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is....$G Success $N" | tee -a $LOG_FILE
+    fi
+}
+
+echo "Script is started using at: $(date)" | tee -a $LOG_FILE
+
+
+CHECK_ROOT
+dnf install nginx -y 
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx
+VALIDATE $? "Enabling Nginx"
+
+systemctl start nginx
+VALIDATE $? "Starting Nginx"
+
+rm -rf /usr/share/nginx/html/*
+VALIDATE $? "Removing Default Website"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+VALIDATE $? "Download Frontend Code"
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
+VALIDATE $? "Unzipping Froentend Code"
+
+cp /home/ec2-user/temp/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "Copying expence conf"
+
+systemctl restart nginx
+VALIDATE $? "Restarting Nginx"
