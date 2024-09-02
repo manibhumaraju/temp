578aa1117a5a9af66cd09cff032840708444fb62
manibhumaraju <manishvarmab@gmail.com>
2024-09-02 17:49:57 +0530

    shell

--- frontend.sh
+++ frontend.sh
@@ -6,7 +6,7 @@ LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOG_FOLDER
+mkdir -p $LOGS_FOLDER
 
 R="\e[31m"
 G="\e[32m"
