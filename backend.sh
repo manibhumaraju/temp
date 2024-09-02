60ce417b9036747718bdcd203afdc57e14c01e6d
manibhumaraju <manishvarmab@gmail.com>
2024-09-02 17:08:09 +0530

    shell

--- backend.sh
+++ backend.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOG_FOLDER="/var/log/expense"
+LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOG_FOLDER
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
 
 R="\e[31m"
 G="\e[32m"
--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOG_FOLDER="/var/log/expense"
+LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOG_FOLDER
 
 R="\e[31m"
