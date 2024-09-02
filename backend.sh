1b45c8f390fe1f068d3d72370c8a54899c5ba901
manibhumaraju <manishvarmab@gmail.com>
2024-09-02 17:05:35 +0530

    shell

--- backend.sh
+++ backend.sh
@@ -7,11 +7,16 @@ TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOG_FOLDER
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 
 CHECK_ROOT=(){
     if [ $USERID -ne 0 ]
     then
-        echo "PLEASE RUN THIS COMMAND WIRH ROOT PRIVILEGES"
+        echo -e "$R PLEASE RUN THIS COMMAND WIRH ROOT PRIVILEGES $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -19,58 +24,58 @@ CHECK_ROOT=(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 is....Failed"
+        echo -e "$2 is....$R Failed $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo "$2 is....Success"
+        echo -e "$2 is....$G Success $N" | tee -a $LOG_FILE
     fi
 }
 
 CHECK_ROOT
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$LOG_FILE
 VALIDATE $? "Disableing nodejs"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>$LOG_FILE
 VALIDATE $? "Enabling nodejs"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing nodejs"
-id expense
+id expense &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    echo "Expense user is not exist...going to Creating"
-    useradd expense
+    echo "Expense user is not exist...going to Creating" &>>$LOG_FILE
+    useradd expense &>>$LOG_FILE
     VALIDATE $? "adding Expence user"
 else
-    echo "Expense user is already exist..Skipping"
+    echo "Expense user is already exist..$Y Skipping $N"
 fi
 
-mkdir -p /app
+mkdir -p /app &>>$LOG_FILE
 VALIDATE $? "Creating app folder"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
 VALIDATE $? "Downloding Backend Code"
 
-cd /app
-rm -rf /app/*
-unzip /tmp/backend.zip
+cd /app &>>$LOG_FILE
+rm -rf /app/* &>>$LOG_FILE
+unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting Backend Code"
 
-cd /app
-npm install
+cd /app &>>$LOG_FILE
+npm install &>>$LOG_FILE
 cp /home/ec2-user/temp/backend.service /etc/systemd/system/backend.service
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Installing MySQL"
 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
 VALIDATE $? "Loading Schema"
 
-systemctl daemon-reload
+systemctl daemon-reload &>>$LOG_FILE
 VALIDATE $? "Deamon Reloded"
 
-systemctl enable backend
+systemctl enable backend &>>$LOG_FILE
 VALIDATE $? "Enable Backend"
 
-systemctl restart backend
+systemctl restart backend &>>$LOG_FILE
 VALIDATE $? "Restart Backend"
