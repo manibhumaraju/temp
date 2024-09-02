4df0cf091a8f2207e444e677f5ca4787e4c252dd
manibhumaraju <manishvarmab@gmail.com>
2024-09-02 16:22:20 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 USERID=$(id -u)
+LOG_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e"[0m"
+mkdir -p LOG_FOLDER
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo "Please run this Script with root priveleges"
+        echo -e "$R Please run this Script with root priveleges $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -12,33 +22,34 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 is Failed.."
+        echo -e "$2 is....$R Failed$N" | tee -a $LOG_FILE
     else
-        echo "$2 is Success.."
+        echo -e "$2 is....$G Success$N" | tee -a $LOG_FILE
     fi
 }
 
-echo "Script is started using at: $(date)"
+echo "Script is started using at: $(date)" | tee -a $LOG__FILE
+
 
 CHECK_ROOT
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$LOG_FILE
 VALIDATE $? "Installing MySQL-Server"
 
-systemctl enable mysqld  
+systemctl enable mysqld &>>$LOG_FILE
 VALIDATE $? "Enabling MySQL"
 
-systemctl start mysqld
+systemctl start mysqld &>>$LOG_FILE
 VALIDATE $? "Starting MySQL"
 
-mysql -h manibhumaraju.online -u root -pExpenseApp@1 -e 'show databases;'
-if [ $? -ne 0]
+mysql -h manibhumaraju.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+if [ $? -ne 0 ]
 then
-    echo "Mysql Root password is not set... creating now"
+    echo "Mysql Root password is not set... Setting now" &>>$LOG_FILE
     mysql_secure_installation --set-root-pass ExpenseApp@1
     VALIDATE $? "Setting MySQL Root Password"
 else
-    echo "MySQL Root Password is already Setup...SKIPPING" 
+    echo "MySQL Root Password is already Setup...SKIPPING" | tee -a $LOG_FILE
 fi
 
 
